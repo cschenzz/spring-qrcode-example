868678c57328f2d582efb52fddb700d4d1802a44
chenzz <cs.chenzz@qq.com>
2019-08-29 17:15:27 +0800

    升级spring boot版本

--- src/main/java/com/github/spring/example/SpringExampleApp.java
+++ src/main/java/com/github/spring/example/SpringExampleApp.java
@@ -15,20 +15,27 @@
  */
 package com.github.spring.example;
 
-import java.util.concurrent.TimeUnit;
-
-import org.springframework.beans.factory.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
-import org.springframework.http.*;
-import org.springframework.scheduling.annotation.*;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.server.*;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+
+import java.util.concurrent.TimeUnit;
+
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
@@ -40,38 +47,38 @@ import static org.springframework.web.reactive.function.server.ServerResponse.ok
 @CrossOrigin(methods = {RequestMethod.GET, RequestMethod.DELETE})
 public class SpringExampleApp {
 
-	public static final String QRCODE_ENDPOINT = "/qrcode";
-	public static final long THIRTY_MINUTES = 1800000; 
+    public static final String QRCODE_ENDPOINT = "/qrcode";
+    public static final long THIRTY_MINUTES = 1800000;
 
-	@Autowired
-	ImageService imageService;
+    @Autowired
+    ImageService imageService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringExampleApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExampleApp.class, args);
+    }
 
-	@GetMapping(value = QRCODE_ENDPOINT, produces = MediaType.IMAGE_PNG_VALUE)
-	public Mono<ResponseEntity<byte[]>> getQRCode(@RequestParam(value = "text", required = true) String text) {
-		return imageService.generateQRCode(text, 256, 256).map(imageBuff ->  
-			ResponseEntity.ok().cacheControl(CacheControl.maxAge(30, TimeUnit.MINUTES)).body(imageBuff)
-		);
-	}
+    @GetMapping(value = QRCODE_ENDPOINT, produces = MediaType.IMAGE_PNG_VALUE)
+    public Mono<ResponseEntity<byte[]>> getQRCode(@RequestParam(value = "text", required = true) String text) {
+        return imageService.generateQRCode(text, 256, 256).map(imageBuff ->
+                ResponseEntity.ok().cacheControl(CacheControl.maxAge(30, TimeUnit.MINUTES)).body(imageBuff)
+        );
+    }
 
-	@Scheduled(fixedRate = THIRTY_MINUTES)
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	@DeleteMapping(value = QRCODE_ENDPOINT)
-	public void deleteAllCachedImages() {
-		imageService.purgeCache();
-	}
+    @Scheduled(fixedRate = THIRTY_MINUTES)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = QRCODE_ENDPOINT)
+    public void deleteAllCachedImages() {
+        imageService.purgeCache();
+    }
 
-	// Workaround, see: https://github.com/spring-projects/spring-boot/issues/9785
-	@Bean
-	public RouterFunction<ServerResponse> indexRouter(@Value("classpath:/static/index.html") final Resource indexHtml) {
-		return route(GET("/"), request -> ok().contentType(MediaType.TEXT_HTML).syncBody(indexHtml));
-	}
+    // Workaround, see: https://github.com/spring-projects/spring-boot/issues/9785
+    @Bean
+    public RouterFunction<ServerResponse> indexRouter(@Value("classpath:/static/index.html") final Resource indexHtml) {
+        return route(GET("/"), request -> ok().contentType(MediaType.TEXT_HTML).syncBody(indexHtml));
+    }
 
-	@ExceptionHandler(RuntimeException.class)
-	public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
-		return ResponseEntity.status(500).contentType(MediaType.TEXT_PLAIN).body(ex.getMessage());
-	}
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(500).contentType(MediaType.TEXT_PLAIN).body(ex.getMessage());
+    }
 }
